f30a363ebb5d4c466e18e33bf34e2f32fab9d5ba
primo <monkarage@gmail.com>
2020-02-10 18:12:22 +0200

    Django Learning

---
+++ populate_tst_app.py
@@ -0,0 +1,41 @@
+import os
+import django
+import random
+from tstApp.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTestProject.settings')
+
+django.setup()
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+    for entry in range(N):
+        # get topic from the entry
+        top = add_topic()
+
+        # Create the fake data for that entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.name()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        # Create a fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+
+
+if __name__ == '__main__':
+    print('populating script!')
+    populate(20)
+    print("populating complete!")
